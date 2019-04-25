bc477f165d90d1bc7150161f99b65ef518ef5c5c
Alireza Oroumand <ar.oroumand@gmail.com>
2019-04-25 15:29:33 +0430

    Add TagHelper For Paging

--- NikamoozStore.EndPoints.WebUI/TagHelpers/PageLinkTagHelper.cs
+++ NikamoozStore.EndPoints.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -1,16 +1,57 @@
-﻿//using Microsoft.AspNetCore.Razor.TagHelpers;
-//using NikamoozStore.EndPoints.WebUI.Models.Commons;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using NikamoozStore.EndPoints.WebUI.Models.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace NikamoozStore.EndPoints.WebUI.TagHelpers
-//{
+namespace NikamoozStore.EndPoints.WebUI.TagHelpers
+{
 
-//    [HtmlTargetElement("div", Attributes = "page-model")]
-//    public class PageLinkTagHelper:TagHelper
-//    {
-//        public PagingInfo PageModel { get; set; }
-//    }
-//}
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+= new Dictionary<string, object>();
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            //TagBuilder result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["pageNumber"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+                //tag.Attributes["href"] = urlHelper.Action(PageAction,new { pageNumber = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                output.Content.AppendHtml(tag);
+            }
+        }
+    }
+}
