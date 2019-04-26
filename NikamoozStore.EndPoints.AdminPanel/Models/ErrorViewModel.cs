002720a6a157a3dfbb892d8375152f60dcf6f82a
Alireza Oroumand <ar.oroumand@gmail.com>
2019-04-26 14:32:24 +0430

    Add Admin Panel

---
+++ NikamoozStore.EndPoints.AdminPanel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NikamoozStore.EndPoints.AdminPanel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
